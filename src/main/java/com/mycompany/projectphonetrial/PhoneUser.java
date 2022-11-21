892025bce650e1de76172d3706409001d908fa62
putrieka <putriekanuralifah341@gmail.com>
2022-11-21 17:35:30 +0700

    Komentar

---
+++ src/main/java/com/mycompany/projectphonetrial/PhoneUser.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectphonetrial;
+
+/**
+ *
+ * @author acer
+ * PUTRI EKA NUR ALIFAH
+ * 21103025
+ * S1SI05A
+ */
+public class PhoneUser {
+     private Phone phone;
+
+    public PhoneUser(Phone phone) {
+        this.phone = phone;
+    }
+    
+    void turnOnThePhone(){
+        this.phone.powerOn();
+    }
+    
+    void turnOffThePhone(){
+        this.phone.powerOff();
+    }
+    
+    void makePhoneLouder(){
+        this.phone.volumeUp();
+    }
+    
+    void makePhoneSilent(){
+        this.phone.volumeDown();
+    }
+}
